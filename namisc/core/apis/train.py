d4799ec8d8b46d01fe0c9b37f8fbe89681ea783c
LOTEAT <zenglezhu@gmail.com>
2023-06-19 23:40:58 +0800

    connection

--- namisc/core/apis/train.py
+++ namisc/core/apis/train.py
@@ -2,11 +2,8 @@
 Author: LOTEAT
 Date: 2023-06-19 11:55:41
 '''
-# import os
-# import warnings
-
-# import torch
-from mmcv.parallel import MMDataParallel, MMDistributedDataParallel, collate
+import os
+from mmcv.parallel import MMDataParallel
 
 from namisc.models.builder import build_transceiver
 from namisc.utils import get_root_logger
@@ -38,34 +35,34 @@ def train_sc(cfg):
     #         broadcast_buffers=False,
     #         find_unused_parameters=find_unused_parameters)
     # else:
-    network = MMDataParallel(network.cuda(), device_ids=[1])
+    tranceiver = MMDataParallel(tranceiver.cuda(), device_ids=[1])
 
     Runner = get_runner(cfg.train_runner)
-    runner = Runner(network,
+    runner = Runner(tranceiver,
                     optimizer=optimizer,
                     work_dir=cfg.work_dir,
                     logger=get_root_logger(log_level=cfg.log_level),
                     meta=None)
 
-    # runner.timestamp = cfg.get('timestamp', None)
+    runner.timestamp = cfg.get('timestamp', None)
 
-    # # register hooks
-    # print('register hooks...', flush=True)
-    # custom_hooks = cfg.get('custom_hooks', None)
-    # print(custom_hooks)
-    # runner.register_training_hooks(cfg.lr_config,
-    #                                cfg.optimizer_config,
-    #                                cfg.checkpoint_config,
-    #                                cfg.log_config,
-    #                                custom_hooks_config=custom_hooks)
-    # register_hooks(cfg.train_hooks, **locals())
+    # register hooks
+    print('register hooks...', flush=True)
+    custom_hooks = cfg.get('custom_hooks', None)
+    print(custom_hooks)
+    runner.register_training_hooks(cfg.lr_config,
+                                   cfg.optimizer_config,
+                                   cfg.checkpoint_config,
+                                   cfg.log_config,
+                                   custom_hooks_config=custom_hooks)
+    register_hooks(cfg.train_hooks, **locals())
 
-    # # resume_from是载入ckpt和runner的训练信息，load_checkpoint只载入ckpt
-    # if cfg.get('resume_from', None):
-    #     runner.resume(cfg.resume_from)
-    # elif cfg.get('load_from', None) and os.path.exists(cfg.load_from):
-    #     runner.load_checkpoint(cfg.load_from)
-    # runner_kwargs = dict()
+    # resume_from是载入ckpt和runner的训练信息，load_checkpoint只载入ckpt
+    if cfg.get('resume_from', None):
+        runner.resume(cfg.resume_from)
+    elif cfg.get('load_from', None) and os.path.exists(cfg.load_from):
+        runner.load_checkpoint(cfg.load_from)
+    runner_kwargs = dict()
 
-    # print('start train...', flush=True)
-    # runner.run(dataloaders, cfg.workflow, cfg.max_iters, **runner_kwargs)
+    print('start train...', flush=True)
+    runner.run(dataloaders, cfg.workflow, cfg.max_iters, **runner_kwargs)
