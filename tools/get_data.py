ca73094c147531855698f9a31793489302fc8cfd
LOTEAT <zenglezhu@gmail.com>
2023-06-28 19:30:21 +0800

    second draft

--- tools/get_data.py
+++ tools/get_data.py
@@ -13,9 +13,28 @@ data_root = 'data'
 data_info = {
     'europarl': {
         'url': 'http://www.statmt.org/europarl/v7/europarl.tgz',
-        'filename': 'europarl.tgz'
+        'filename': 'europarl.tgz',
+        'format': 'tgz'
+    }, 
+    'DataShare_clean_testset': {
+        'url': 'https://datashare.ed.ac.uk/bitstream/handle/10283/2791/clean_testset_wav.zip?sequence=1&isAllowed=y',
+        'filename': 'clean_testset_wav.zip',
+        'format': 'zip'
     }
+    
 }
+
+
+def extract(filepath, target_dir, format):
+    if format == 'tgz':
+        with tarfile.open(filepath, 'r:gz') as tar:
+            members = tar.getmembers()
+            progress = tqdm(members, desc='Extracting', ncols=80, leave=False)
+            for member in progress:
+                tar.extract(member, path=target_dir)
+                progress.set_postfix(file=member.name)
+                progress.update(1)
+
 
 
 def download(url, filename):
@@ -25,18 +44,11 @@ def download(url, filename):
     with tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024, desc=filename, ncols=80) as pbar:
         urllib.request.urlretrieve(url, filepath, reporthook=lambda b, bsize, tsize: pbar.update(bsize))
     target_dir = osp.join(data_root, filename)
-    with tarfile.open(filepath, 'r:gz') as tar:
-        members = tar.getmembers()
-        progress = tqdm(members, desc='Extracting', ncols=80, leave=False)
-        for member in progress:
-            tar.extract(member, path=target_dir)
-            progress.set_postfix(file=member.name)
-            progress.update(1)
-    print("Sucessfully downloaded!")
+    extract(filepath, target_dir, data_info[filename]['format'])
+    print("Finished!")
 
 
 def get_data(data_name):
-    if data_name == 'europarl':
-        download(data_info[data_name]['url'], data_info[data_name]['filename'])
+    download(data_info[data_name]['url'], data_info[data_name]['filename'])
 
-get_data('europarl')
+get_data('DataShare_clean_testset')
